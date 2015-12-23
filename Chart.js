810f2c3859cbb62d20ec1363a3253f89f4fd4b96
Grant Thompson <thlgrant@users.noreply.github.com>
2015-12-23 02:00:46 +0000

    Update Chart.js

--- Chart.js
+++ Chart.js
@@ -2131,18 +2131,18 @@
 
 				this.datasets.push(datasetObject);
 
-				helpers.each(dataset.data,function(dataPoint,index){
-					//Add a new point for each piece of data, passing any required data to draw.
-					datasetObject.bars.push(new this.BarClass({
-						value : dataPoint,
-						label : data.labels[index],
-						datasetLabel: dataset.label,
-						strokeColor : dataset.strokeColor,
-						fillColor : dataset.fillColor,
-						highlightFill : dataset.highlightFill || dataset.fillColor,
-						highlightStroke : dataset.highlightStroke || dataset.strokeColor
-					}));
-				},this);
+				helpers.each(dataset.data, function(dataPoint, index) {
+				    //Add a new point for each piece of data, passing any required data to draw.
+				    datasetObject.bars.push(new this.BarClass({
+				        value: dataPoint,
+				        label: data.labels[index],
+				        datasetLabel: dataset.label,
+				        strokeColor: dataset.strokeColor,
+				        fillColor: dataset.fillColor[index] || dataset.fillColor, //<--getting color using index
+				        highlightFill: dataset.highlightFill || dataset.fillColor,
+				        highlightStroke: dataset.highlightStroke || dataset.strokeColor
+				    }));
+				}, this);
 
 			},this);
 
